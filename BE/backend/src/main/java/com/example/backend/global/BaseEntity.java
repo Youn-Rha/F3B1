b0177fcdf1cbb586fbb2186d92b5c7bbd561da9a
milk-stone <ywshero@naver.com>
2024-06-02 01:48:10 +0900

    feat : BaseEntity

--- BE/backend/src/main/java/com/example/backend/global/BaseEntity.java
+++ BE/backend/src/main/java/com/example/backend/global/BaseEntity.java
@@ -1,4 +1,23 @@
 package com.example.backend.global;
 
+import jakarta.persistence.EntityListeners;
+import jakarta.persistence.MappedSuperclass;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.annotation.CreatedBy;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import java.time.LocalDateTime;
+
+@Configuration
+@MappedSuperclass
+@EntityListeners(AuditingEntityListener.class)
 public class BaseEntity {
+    @CreatedDate
+    private LocalDateTime createdAt;
+
+    @LastModifiedDate
+    private LocalDateTime updatedAt;
+
 }
